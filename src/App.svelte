55ef360d29f3f566ae4393baac14138739e1ffd7
Seppo Kilpelainen <seppo.kilpelainen@gmail.com>
2020-10-12 17:57:52 +0300

    centered the main content outside of the router

--- src/App.svelte
+++ src/App.svelte
@@ -5,4 +5,25 @@
   import { routes } from "./routes";
 </script>
 
-<Router {routes} />
+<style>
+  #flex-parent {
+    display: flex;
+  }
+  .side {
+    flex-shrink: 1;
+    flex-grow: 1;
+    min-width: 1em;
+  }
+  #main {
+    flex-basis: 800px;
+    min-width: 360px;
+  }
+</style>
+
+<div id="flex-parent">
+  <div class="side" />
+  <div id="main">
+    <Router {routes} />
+  </div>
+  <div class="side" />
+</div>
